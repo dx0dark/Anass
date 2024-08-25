31f8e997f3bca030d9413dd4dca95ef810237fc2
jamilaanas <maryamstatut@gmail.com>
2024-08-25 19:27:42 +0100

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -40,7 +40,7 @@ try{
 
   await m.react('✅');
   let video = data.url;
-  let dev = '© 2024 Queen Anju Tiktok Downloader | Download with ease, cherish forever.'
+  let dev = '© 2024 Anas En Najjari Tiktok Downloader | Download with ease, cherish forever.'
   
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'tt.mp4', mimetype: 'video/mp4' }, { quoted: m });
