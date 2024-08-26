15cc3ee14a009f5d56617588f1d7bb62e7c8faee
Anassnajjari12 <109917832+Anassnajjari12@users.noreply.github.com>
2024-08-26 09:36:14 +0100

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> QUEEN ANJU-MD' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> Anas En Najjari' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error fetching dog image: ${e.message}`);
