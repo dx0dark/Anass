a7e15209305fd17d895c67084fa9551d472d1db1
Anassnajjari12 <109917832+Anassnajjari12@users.noreply.github.com>
2024-08-26 09:26:59 +0100

    Update gpass.js

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         };
 
         const password = generatePassword(length);
-        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n> QUEEN ANJU-MD`;
+        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n> Anas En Najjari`;
 
         // Send initial notification message
         await conn.sendMessage(from, { text: message }, { quoted: mek });
