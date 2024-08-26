b3466672a8e6e8004ac25f757f2d3a53cf958b8d
Anassnajjari12 <109917832+Anassnajjari12@users.noreply.github.com>
2024-08-26 09:40:36 +0100

    Update rvideo.js

--- plugins/rvideo.js
+++ plugins/rvideo.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         writer.on('finish', async () => {
             await conn.sendMessage(from, { text: '✅ *Your video has been successfully downloaded!* ✅' }, { quoted: mek });
-            await conn.sendMessage(from, { video: { url: videoPath }, caption: `🎥 *Random Pexels Video* 🎥\n\nTitle: ${videoTitle}\n> QUEEN ANJU-MD` }, { quoted: mek });
+            await conn.sendMessage(from, { video: { url: videoPath }, caption: `🎥 *Random Pexels Video* 🎥\n\nTitle: ${videoTitle}\n> Anas En Najjari` }, { quoted: mek });
 
             // Clean up
             fs.unlinkSync(videoPath);
